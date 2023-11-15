0df9ce33ae6145eef3040979a417e708d590b520
Myriam Mabrouki <myriam.mabrouki@tutanota.com>
2023-11-15 01:50:34 +0100

    echauffement correct

---
+++ Key.hh
@@ -0,0 +1,56 @@
+#pragma once
+
+//#include <cstdlib>
+#include <vector>
+#include <string>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+namespace algav {
+
+	class Key {
+
+	private:
+		std::vector<unsigned long> key;
+
+	public:
+		Key(std::string hex){
+			hex = hex.substr(2, hex.size());
+			std::string tmp;
+			for (size_t i = 0; i < 4; ++i){
+				tmp = hex.substr(i*8, 8);
+				unsigned long value;
+				std::istringstream iss(tmp);
+				iss >> std::hex >> value;
+				key.emplace_back(value);
+			}
+		}
+
+		~Key(){}
+
+		bool inf (Key & other) {
+			for (size_t i = 0; i < 4; ++i){
+				if (key[i] < other.key[i]) return true;
+				if (key[i] > other.key[i]) return false;
+			}
+			return false;
+		}
+
+		bool eg (Key & other) {
+			return other.key == key;
+		}
+
+		friend std::ostream & operator << (std::ostream & os, const Key & k){
+			for (size_t i = 0; i < 4; ++i){
+				os << std::hex << k.key[i];
+			}
+			return os;
+		}
+
+
+	};
+
+}
+
---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include "Key.hh"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace algav;
+using namespace std;
+
+
+vector<Key> readKeysFromFile(const std::string& filepath) {
+    std::ifstream file(filepath);
+    std::string line;
+    std::vector<Key> keys;
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filepath << std::endl;
+        return keys;
+    }
+
+    // Read key from the file one by one
+    while (file >> line) {
+    	keys.emplace_back(line);
+    }
+
+    file.close();
+
+    return keys;
+}
+
+int main(int argc, char ** argv) {
+
+	if (argc != 2) {
+		cerr << "Error wrong number of arguments: " << endl;
+		return 1;
+	}
+
+	string filename = argv[1];
+	vector<Key> keys = readKeysFromFile(filename);
+
+	for (Key & k : keys){
+		cout << k << endl;
+	}
+
+	for (size_t i = 0; i < keys.size()-1; ++i){
+		if (keys[i].eg(keys[i+1])){
+			cout << keys[i] << " = " << keys[i+1] << endl;
+		}
+		else if (keys[i].inf(keys[i+1])) {
+			cout << keys[i] << " < " << keys[i+1] << endl;
+		}
+		else {
+			cout << keys[i] << " > " << keys[i+1] << endl;
+		}
+	}
+
+	return 0;
+
+}
